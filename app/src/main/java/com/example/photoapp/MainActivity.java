6f6d9ca9716fdc99fb0aa27abdcad3c6823f5c7e
br-eina <bruenor11@gmail.com>
2019-03-31 20:02:20 +0300

    redacted, shitty UI * take a pic * save in a picture/ directory

--- app/src/main/java/com/example/photoapp/MainActivity.java
+++ app/src/main/java/com/example/photoapp/MainActivity.java
@@ -3,23 +3,16 @@ package com.example.photoapp;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
-
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -27,16 +20,12 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int CAMERA_REQUEST_CODE = 322;
     public static final int CAMERA_PERMISSION_REQUEST_CODE = 1337;
-    public static final int IMAGE_GALLERY_REQUEST = 20;
 
-    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        img = findViewById(R.id.img);
     }
 
     /**
@@ -44,26 +33,13 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void btnTakePhotoClicked(View view) {
-
         if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
             invokeCamera();
-
         } else {
             // let's request permission
             String[] permissionRequest = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
             requestPermissions(permissionRequest, CAMERA_PERMISSION_REQUEST_CODE);
-
         }
-
-//        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-//        String pictureName = getPictureName();
-//        File imageFile = new File(pictureDirectory, pictureName);
-//        Uri pictureUri = Uri.fromFile(imageFile);
-//        // 1 arg - want to save, 2 arg - where to save
-//        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);
-//        startActivityForResult(cameraIntent, CAMERA_REQUEST);
-
     }
 
     @Override
@@ -75,27 +51,21 @@ public class MainActivity extends AppCompatActivity {
                 invokeCamera();
             } else {
                 Toast.makeText(this, getString(R.string.cannotopencamera), Toast.LENGTH_LONG).show();
-
             }
-
         }
     }
 
     private void invokeCamera() {
         // get a file reference
         Uri pictureUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", createImageFile());
-
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
         // tell the camera where to save the image
         intent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);
-
         // tell the camera to request right permission
         intent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
         startActivityForResult(intent, CAMERA_REQUEST_CODE);
 
     }
-
 
     private File createImageFile() {
         // the public picture directory
@@ -105,18 +75,8 @@ public class MainActivity extends AppCompatActivity {
         String timeStamp = sdf.format(new Date());
         // put together the directory and the timestamp
         File imageFile = new File(picturesDirectory, "picture" + timeStamp + ".jpg");
-
         return imageFile;
-
-
     }
-
-//        private String getPictureName() {
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
-//        String timeStamp = sdf.format(new Date());
-//        return "photo22" + timeStamp + ".jpg";
-//
-//    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -124,37 +84,56 @@ public class MainActivity extends AppCompatActivity {
             if (requestCode == CAMERA_REQUEST_CODE) {
                 Toast.makeText(this, "Image Saved.", Toast.LENGTH_LONG).show();
             }
-            // if we are here, everything processed successfully.
-            if (requestCode == IMAGE_GALLERY_REQUEST) {
-                // if we are here, we are hearing back from the image gallery.
-
-                // the address of the image on the SD Card.
-                Uri imageUri = data.getData();
-
-                // declare a stream to read the image data from the SD Card.
-                InputStream inputStream;
-
-                // we are getting an input stream, based on the URI of the image.
-                try {
-                    inputStream = getContentResolver().openInputStream(imageUri);
-
-                    // get a bitmap from the stream.
-                    Bitmap image = BitmapFactory.decodeStream(inputStream);
-
-
-                    // show the image to the user
+//             if we are here, everything processed successfully.
+//            if (requestCode == IMAGE_GALLERY_REQUEST) {
+//                // if we are here, we are hearing back from the image gallery.
+//
+//                // the address of the image on the SD Card.
+//                Uri imageUri = data.getData();
+//
+//                // declare a stream to read the image data from the SD Card.
+//                InputStream inputStream;
+//
+//                // we are getting an input stream, based on the URI of the image.
+//                try {
+//                    inputStream = getContentResolver().openInputStream(imageUri);
+//
+//                    // get a bitmap from the stream.
+//                    Bitmap image = BitmapFactory.decodeStream(inputStream);
+//
+//
+//                    // show the image to the user
 //                    imgPicture.setImageBitmap(image);
-
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                    // show a message to the user indictating that the image is unavailable.
-                    Toast.makeText(this, "Unable to open image", Toast.LENGTH_LONG).show();
-                }
-
-            }
+//
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                    // show a message to the user indictating that the image is unavailable.
+//                    Toast.makeText(this, "Unable to open image", Toast.LENGTH_LONG).show();
+//                }
+//
+//            }
         }
     }
 
 
 
 }
+//        public static final int IMAGE_GALLERY_REQUEST = 20;
+//        private ImageView img;
+//        img = findViewById(R.id.img);
+
+//        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+//        String pictureName = getPictureName();
+//        File imageFile = new File(pictureDirectory, pictureName);
+//        Uri pictureUri = Uri.fromFile(imageFile);
+//        // 1 arg - want to save, 2 arg - where to save
+//        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);
+//        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+//        private String getPictureName() {
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+//        String timeStamp = sdf.format(new Date());
+//        return "photo22" + timeStamp + ".jpg";
+//
+//    }
